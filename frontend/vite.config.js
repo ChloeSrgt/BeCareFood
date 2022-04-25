55060bd6cf5b6863568e09e927593ab4b1e869a9
delphinestraub <delphine.straub@gmail.com>
2022-04-25 17:44:43 +0200

    Mise à jour pour synchro autres dépôts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const path = require("path");
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@services": path.resolve(__dirname, "src/services"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@assets": path.resolve(__dirname, "src/assets"),
+            "@components": path.resolve(__dirname, "src/components"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+            "@services": path.resolve(__dirname, "src/services"),
+            // eslint-disable-next-line prettier/prettier
+        },
     },
-  },
 });
