7a25d4aa2d03e2e1b50e436a78cd71765aa95cde
Chloe-Sirgent <chloe.sirgent@gmail.com>
2022-04-26 22:46:20 +0200

    creation page produit avec context

--- frontend/src/components/ProductName.jsx
+++ frontend/src/components/ProductName.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { ProductContext } from "../App";
+import "./ProductName.css";
 export default function ProductName() {
-  const { idProduct } = useParams();
-  return <div>{idProduct}</div>;
+  const { product } = useContext(ProductContext);
+
+  return product ? (
+    <div className="result">
+      <h1>{product.product_name}</h1>
+      <img className="productImg" src={product.image_front_url} alt="Product" />
+      <p>
+        {" "}
+        {product.ingredients_text
+          ? product.ingredients_text
+          : "Pas d'ingrédient communiqué"}
+      </p>
+      {product.nutriscore_grade && (
+        <img
+          src={`https://static.openfoodfacts.org/images/attributes/nutriscore-${product.nutriscore_grade}.svg`}
+        />
+      )}
+    </div>
+  ) : (
+    <Navigate to="/" />
+  );
 }
